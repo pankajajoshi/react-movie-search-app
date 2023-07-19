9d888672dffd6cec77ecf338f4d3b092dfdc48e3
pankajajoshi <joshi.pankaja@gmail.com>
2023-07-19 11:20:45 -0400

    try catch block added for handling error

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
+import {useState, useEffect} from "react";
 import './App.css';
-
+import MovieDisplay from './components/MovieDisplay';
+import Form from './components/Form';
 function App() {
+   //variable with your apiKey
+   const apiKey = "3c4f1566";
+    //State to hold movie data
+  const [movie, setMovie] = useState(null);
+
+    //Function to getMovies  //async -> stands for a syncronous you can with for fuction preceeded by 
+    const getMovie = async (searchTerm) => {
+      // make fetch request and store response
+      //fetch is a promise to get data until then await
+     try{
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${apiKey}&t=${searchTerm}`
+      );
+       // Parse JSON response into a javascript object
+    //o/p of response from fetch which will be string obj and cretes json obj created
+       const data = await response.json()
+    //set the Movie state to the movie
+    setMovie(data)
+  }catch(error){
+    console.error(error)
+  }
+}
+  useEffect(() => {
+    getMovie("God Father");
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Form moviesearch={getMovie} />
+     <MovieDisplay movie={movie} />
     </div>
   );
 }
